b1d07384395b1a42ac3798b9990f5fc2ba5fe2c2
Your Name <your.email@example.com>
2026-01-03 10:31:35 +0530

    jsonwebtoken

--- Next2-work/app.js
+++ Next2-work/app.js
@@ -1,17 +1,28 @@
 const cookieParser = require('cookie-parser');
 const express = require('express');
 const app = express();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 app.use(cookieParser());
 
+// app.get("/",function(req,res){
+//     bcrypt.compare("abcdefghijk", "$2b$10$Bq53BSYVcrx02BkciJIfJOTs66zlfuWQmpCZXS8Tpolzl1/2.s5xm", function(err, result) {
+//         console.log(result);
+//     });
+        // Store hash in your password DB.  
+// 
+
 app.get("/",function(req,res){
-    res.cookie("name","harsh");
-    res.send("done");
-})
+   let token = jwt.sign({email:"harsh@example.com"},"secret");
+   res.cookie("token",token);
+   res.send("done");   
+    }); 
+// });
+// })
 
 app.get("/read",function(req,res){
-    console.log(req.cookies);
-    res.send("read page");
+        let data = jwt.verify(req.cookies.token,"secret");
+        console.log(data);
 })
-
 app.listen(3000);
